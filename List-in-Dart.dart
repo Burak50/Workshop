3a7cdca40621a6472daf3fa3db4d96fbca4245c9
Burak50 <69373582+Burak50@users.noreply.github.com>
2020-11-07 16:17:37 +0100

    Create List-in-Dart.dart

---
+++ List-in-Dart.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<String> names =["Deutsch", "Englisch", "Mathematik", "Politik", "Kunst", "Informatik"];
+  
+  for(int i = 0; i < names.length; i++){
+  print(names[0]);
+  print(names[1]);
+  print(names[2]);
+  print(names[3]);
+  print(names[4]);
+  print(names[5]);
+}
+}
