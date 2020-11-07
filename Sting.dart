01a04fb221acefc5cbca4a7abfba38bf5ab4d319
Burak50 <69373582+Burak50@users.noreply.github.com>
2020-11-07 15:00:05 +0100

    Create Sting.dart

---
+++ Sting.dart
@@ -0,0 +1,11 @@
+void main() {
+    String meinName = "Burak";
+  print("Mein Name ist $meinName");
+  
+  double d = 3.0;
+  String doubleString = d.toString();
+  print(doubleString);
+  int meinNamelength = meinName.length;
+  print("Die Länge meines Name ist $meinNamelength");
+}
+
